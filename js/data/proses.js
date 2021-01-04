46e5baf55f3d0a1282254d4c197d61a21f44bd37
abideditiyaz <abideditiyaz@gmail.com>
2021-01-04 18:14:31 +0700

    adding more bug i think

--- js/data/proses.js
+++ js/data/proses.js
@@ -5,9 +5,15 @@ function DataProcess(onSuccess, onFailed) {
 
 DataProcess.prototype.searchStores = function(keyword) {
 	const filteredStores = stores.filter(stores => stores.address.postalCode.toUpperCase().includes(keyword.toUpperCase()));
+	const filteredStoresName = stores.filter(stores => stores.name.toUpperCase().includes(keyword.toUpperCase()));
+	const filteredStoresAddress = stores.filter(stores => stores.addressLines[0].toUpperCase().includes(keyword.toUpperCase()));
 
 	if (filteredStores.length) {
 		this.onSuccess(filteredStores);
+	}else if (filteredStoresName) {
+		this.onSuccess(filteredStoresName)
+	}else if (filteredStoresAddress) {
+		this.onSuccess(filteredStoresAddress)
 	}else{
 		this.onFailed(`${keyword} doesn't match!!`);
 	}
