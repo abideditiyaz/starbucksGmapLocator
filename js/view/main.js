49d65b2b95a6459aa6a2e09609b01858cc89e32d
abideditiyaz <abideditiyaz@gmail.com>
2021-01-07 17:09:56 +0700

    responsive more

--- js/view/main.js
+++ js/view/main.js
@@ -14,15 +14,18 @@ const main = _ =>{
 			const {streetAddressLine1, city, countrySubdivisionCode} = address;
 
 			const storeElement = document.createElement('div');
-			storeElement.setAttribute('class', 'h-28');
+			storeElement.setAttribute('class', 'h-28 mb-4 p-1');
 
 			storeElement.innerHTML = `
 				<div class="flex flex-col justify-evenly h-full p-2">
-                    <div class="address">
+                    <div class="font-medium">
                         <p>${streetAddressLine1}</p>
-                        <p>${city}, ${countrySubdivisionCode}</p>
+                        <p class="font-light">${city}, ${countrySubdivisionCode}</p>
                     </div>
-                    <div class="phone-number"><p>${phoneNumber}</p></div>
+                    <div class="flex items-center w-full text-white mt-3.5 w-full bg-green-700 pt-1 pb-1 rounded-md justify-center lg:w-3/5">
+                    	<i class="fas fa-phone-alt mr-3"></i>
+                    	<p>${phoneNumber}</p>
+                    </div>
                     <hr>
                 </div>`;
             storeList.appendChild(storeElement);
@@ -59,22 +62,22 @@ const main = _ =>{
 			const {phoneNumber, addressLines, brandName, name, openStatusText} = store;
 
 			const storeElement = document.createElement('div');
-			storeElement.setAttribute('class', 'h-48');
+			storeElement.setAttribute('class', 'h-52 lg:h-60');
 
 			storeElement.innerHTML = `
 				<div class="h-full flex flex-col justify-evenly p-4">
-                    <div class="flex flex-col text-xl font-semibold">
-                    	<span>${brandName}</span>
-                    	<span>${name}</span>
+                    <div class="flex flex-col text-lg">
+                    	<span class="font-semibold lg:text-2xl lg:mb-2">${brandName}</span>
+                    	<span class="text-sm lg:text-xl">${name}</span>
                     </div>
                     <div class="h-full flex flex-col justify-evenly">
                     	<div class="flex items-center w-full">
 	                    	<div class="mr-3">
 	                    		<i class="fas fa-location-arrow"></i>
 	                    	</div>
-	                    	<p>${addressLines}</p>
+	                    	<p class="text-xs lg:text-base">${addressLines}</p>
 	                    </div>
-                    	<span class="block w-full bg-green-800 rounded-xs text-white p-1 text-center">${openStatusText}</span>
+                    	<span class="block w-full bg-green-800 rounded-sm text-xs text-white p-1 text-center lg:text-lg lg:p-3">${openStatusText}</span>
                     </div>
                 </div>
                 <hr>`;
